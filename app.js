fc0950e968962798b0ae557b1345f8cd94e4089f
GuRuGu <peter.k.nets@gmail.com>
2020-01-07 08:12:54 +0200

    Connect to database

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const config = require("config");
 
 const app = express();
 
+// Connect database
+mongoose.connect(config.get("mongodb"), {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.connection.on(
+  "error",
+  console.error.bind(console, "There was a database error")
+);
+mongoose.connection.once("open", () => console.log("Connected to database"));
+
+// Initialize middleware
+app.use(morgan("dev"));
+app.use(express.json());
+
+// Run server
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
